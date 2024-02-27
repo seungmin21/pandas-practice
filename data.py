68962614ee3eb1cdfc8d0293738aa5ec3f173f13
ohseungmin <ohmin2886@gmail.com>
2024-02-27 18:36:06 +0900

    CSV 파일에서 South Korean 데이터만 추출하기

--- data.py
+++ data.py
@@ -4,8 +4,17 @@ import pandas as pd
 csv_file = './netflix_titles.csv'
 
 
+# with open(csv_file, 'r', encoding='utf-8') as file:
+    # reader = file
+    # for row in reader:
+        # print(row)
+
+# CSV 파일 열기
 with open(csv_file, 'r', encoding='utf-8') as file:
-    reader = file
+    reader = csv.DictReader(file)
     for row in reader:
-        print(row)
+        if 'South Korea' in row['country']:
+            print(row)
+
+print(csv_file)
 

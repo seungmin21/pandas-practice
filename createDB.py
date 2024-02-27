be0f9584e8befe3e4b512e92406d235d8f370d68
ohseungmin <ohmin2886@gmail.com>
2024-02-27 14:26:26 +0900

    executescript는 생성 벨류 추가 선택까지의 과정을 순서대로 한번에 진행할 수 있는 SQL 문이다.

--- createDB.py
+++ createDB.py
@@ -8,14 +8,20 @@ conn = sqlite3.connect("database.db")
 cur = conn.cursor()
 
 # 생성
-cur.execute("CREATE TABLE student(id, name, age)")
+#cur.execute("CREATE TABLE student(id, name, age)")
 
 # value 생성
-cur.execute("INSERT INTO student values('5', 'oh', 55)")
+#cur.execute("INSERT INTO student values('5', 'oh', 55)")
 
 # 선택
-cur.execute("SELECT * FROM student")
+#cur.execute("SELECT * FROM student")
 
+#insert_query = "INSERT INTO student (id, name, age) VALUES (?, ?, ?);"
+#new_data = [('4', 'min', 25)]
+#cur.executemany(insert_query, new_data)
+
+# 순서대로 실행하는 SQL 문
+cur.executescript("BEGIN; CREATE TABLE student(id, name, age); INSERT INTO student values('1', 'oh', 15); INSERT INTO student values('2', 'min', 25); SELECT * FROM student; COMMIT;")
 
 conn.close()
 

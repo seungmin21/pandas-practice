ddc9bd0ae8ac9bd59faabd50d2a97b5952ee4946
ohseungmin <ohmin2886@gmail.com>
2024-02-27 18:42:15 +0900

    pandas로도 csv 파일에서 South Korea 데이터만 추출할 수 있는지 시도 실패

--- data.py
+++ data.py
@@ -10,18 +10,28 @@ csv_file = './netflix_titles.csv'
         # print(row)
 
 # CSV 파일 열기
-with open(csv_file, 'r', encoding='utf-8') as file:
-    reader = csv.DictReader(file)
-    for row in reader:
-        if 'South Korea' in row['country']:
-            print(row)
+# with open(csv_file, 'r', encoding='utf-8') as file:
+    # reader = csv.DictReader(file)
+    # for row in reader:
+        # if 'South Korea' in row['country']:
+            # print(row)
 
-print(csv_file)
+# print(csv_file)
 
 data = pd.read_csv('./netflix_titles.csv')
 
-print(data)
-print(data.head())
-print(data.head(20))
-print(data.info())
+# print(data)
+# print(data.head())
+# print(data.head(20))
+# print(data.info())
+
+filtered_data = data['country']
+print(filtered_data)
+
+selected_column = data['country']
+selected_row = data.loc['South_Korea']
+print(selected_row)
+print(selected_column)
+
+
 

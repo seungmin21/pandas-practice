56c7ecd99a5b54679f1ba85244e81c97d436c1a1
ohseungmin <ohmin2886@gmail.com>
2024-04-03 18:36:58 +0900

    결과값 csv 파일을 생성해서 저장 실패

---
+++ ten_data.py
@@ -0,0 +1,32 @@
+import csv
+import pandas as pd
+
+# CSV 파일 로드
+data = pd.read_csv('./netflix_titles.csv')
+
+# "duration" 열의 값을 숫자로 변환하여 추출
+numeric_values = data['duration'].str.extract('(\d+)').astype(float)
+
+# 최대값을 찾고 출력하기
+max_value_row = data.loc[numeric_values.idxmax()]
+print(max_value_row)
+
+# 2번째부터 10번째 최대값까지 찾고 출력하기
+top_10_max_values = []
+for _ in range(9):  # 2번째부터 10번째까지 총 9번 반복
+    # 찾은 최대값을 제외하기
+    max_value = numeric_values.max()
+    numeric_values = numeric_values[numeric_values < max_value]
+    # 다음 최대값 찾기
+    max_value_row = data.loc[numeric_values.idxmax()]
+    # 결과 리스트에 추가
+    top_10_max_values.append(max_value_row)
+
+# 결과를 새로운 데이터프레임으로 생성
+result_df = pd.DataFrame(top_10_max_values)
+
+# 새로운 CSV 파일로 저장
+filename = 'netflix_top10.csv'
+result_df.to_csv(filename, index=False)
+
+print(f"CSV 파일 '{filename}'이 생성되었습니다.")
